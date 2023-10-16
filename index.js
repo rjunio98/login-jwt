46e285a1dbb0f9c1f4b36d8ad9889fef4156c329
rjunio98 <robledorjunior1@gmail.com>
2023-10-16 10:51:39 -0300

    banco de dados

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const userRouter = require("./routes/userRouter");
+const mongoose = require("mongoose");
 
-app.use('/user', userRouter);
+mongoose
+  .connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Conexão com o banco de dados estabelecida.");
+  })
+  .catch((error) => {
+    if (error) console.log(error);
+  });
 
-
-
-
-
-
+app.use("/user", userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server Running");
