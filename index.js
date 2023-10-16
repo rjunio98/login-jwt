358b268ce6f8bd6013605fd2413c8e37223f452e
rjunio98 <robledorjunior1@gmail.com>
2023-10-16 10:02:02 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const app = express();
+const userRouter = require("./routes/userRouter");
+
+app.use('/user', userRouter);
+
+
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log("Server Running");
+});
