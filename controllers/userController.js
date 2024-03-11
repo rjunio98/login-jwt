dfc8417ab50718e9b5d2ca17cd70c470d56adde9
rjunio98 <robledorjunior1@gmail.com>
2024-03-11 18:02:10 -0300

    Adiciona funcionalidade de registro de usuário com tratamento de erros

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,19 @@
+const User = require("../models/User");
+
 const userController = {
-  register: function register(req, res) {
-    res.send("Register");
+  register: async function register(req, res) {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    try {
+      const savedUser = await user.save();
+      res.send(savedUser);
+    } catch (error) {
+      res.status(400).send(error);
+    }
   },
 
   login: function login(req, res) {
