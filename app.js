a13d0dd0ba4a22d55ddea23e25f47fa0f79f1e78
rjunio98 <robledorjunior1@gmail.com>
2024-03-13 15:58:51 -0300

    Adiciona rotas de usuário e administrador e conecta ao banco de dados

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
+const userRouter = require("./routes/userRouter");
+const adminRouter = require("./routes/adminRouter");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.use("/user", express.json(), userRouter);
+
+app.use("/admin", express.json(), adminRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port", process.env.PORT);
